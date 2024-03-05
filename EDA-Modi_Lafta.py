dbe869555e87e6c41a80747253fed575d784eb14
Modi Lafta <132007061+ModiiLafta@users.noreply.github.com>
2024-03-05 10:53:39 +0100

    Add files via upload

---
+++ EDA-Modi_Lafta.py
@@ -0,0 +1,9 @@
+import warnings
+
+warnings.filterwarnings("ignore")
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import missingno as msno
+from sqlalchemy import create_engine
